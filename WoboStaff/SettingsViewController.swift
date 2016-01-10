7d4c68b120c72e8ea514b7aa6ab280f79aebcd39
Alessandro Belardinelli <alessandro.belardinelli@gmail.com>
2016-01-10 13:07:04 -0800

    Adjust settings view position when toggling the keyboard

--- WoboStaff/SettingsViewController.swift
+++ WoboStaff/SettingsViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class SettingsViewController: UIViewController, UITextFieldDelegate {
     
+    // MARK: - Private structs
+    
+    private struct MoveKeyboard
+    {
+        static let keyboardAnimationDuration: CGFloat = 0.3
+        static let minimumScrollFraction: CGFloat = 0.2;
+        static let maximumScrollFraction: CGFloat = 0.8;
+        static let portraitKeyboardHeight: CGFloat = 216;
+        static let landscapeKeyboardHeight: CGFloat = 162;
+    }
+    
     // MARK: - Outlets
 
     @IBOutlet weak var currentTokenLabel: UILabel!
@@ -23,6 +34,10 @@ class SettingsViewController: UIViewController, UITextFieldDelegate {
     {
         UIPasteboard.generalPasteboard().string = currentTokenLabel?.text
     }
+    
+    // MARK: - Private variables
+    
+    private var animateDistance: CGFloat!
 
     // MARK: - Textfields customization
     
@@ -40,6 +55,20 @@ class SettingsViewController: UIViewController, UITextFieldDelegate {
             setNewApiToken()
         }
         return true
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField)
+    {
+        if textField == newTokenTextField {
+            adjustCreateKeyboard(textField)
+        }
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField)
+    {
+        if textField == newTokenTextField {
+            adjustHideKeyboard()
+        }
     }
     
     // MARK: - View lifecycle
@@ -65,6 +94,53 @@ class SettingsViewController: UIViewController, UITextFieldDelegate {
                 navigationController?.popViewControllerAnimated(true)
             }
         }
+    }
+    
+    private func adjustCreateKeyboard(textField: UITextField)
+    {
+        let textFieldRect: CGRect = self.view.window!.convertRect(textField.bounds, fromView: textField)
+        let viewRect: CGRect = self.view.window!.convertRect(self.view.bounds, fromView: self.view)
+        
+        let midline: CGFloat = textFieldRect.origin.y + 0.5 * textFieldRect.size.height
+        let numerator: CGFloat = midline - viewRect.origin.y - MoveKeyboard.minimumScrollFraction * viewRect.size.height
+        let denominator: CGFloat = (MoveKeyboard.maximumScrollFraction - MoveKeyboard.minimumScrollFraction) * viewRect.size.height
+        var heightFraction: CGFloat = numerator / denominator
+        
+        if heightFraction < 0.0 {
+            heightFraction = 0.0
+        } else if heightFraction > 1.0 {
+            heightFraction = 1.0
+        }
+        
+        let orientation: UIInterfaceOrientation = UIApplication.sharedApplication().statusBarOrientation
+        if (orientation == UIInterfaceOrientation.Portrait || orientation == UIInterfaceOrientation.PortraitUpsideDown) {
+            animateDistance = floor(MoveKeyboard.portraitKeyboardHeight * heightFraction)
+        } else {
+            animateDistance = floor(MoveKeyboard.landscapeKeyboardHeight * heightFraction)
+        }
+        
+        var viewFrame: CGRect = self.view.frame
+        viewFrame.origin.y -= animateDistance
+        
+        UIView.beginAnimations(nil, context: nil)
+        UIView.setAnimationBeginsFromCurrentState(true)
+        UIView.setAnimationDuration(NSTimeInterval(MoveKeyboard.keyboardAnimationDuration))
+        
+        self.view.frame = viewFrame
+        UIView.commitAnimations()
+    }
+    
+    private func adjustHideKeyboard()
+    {
+        var viewFrame: CGRect = self.view.frame
+        viewFrame.origin.y += animateDistance
+        
+        UIView.beginAnimations(nil, context: nil)
+        UIView.setAnimationBeginsFromCurrentState(true)
+        UIView.setAnimationDuration(NSTimeInterval(MoveKeyboard.keyboardAnimationDuration))
+        
+        self.view.frame = viewFrame
+        UIView.commitAnimations()
     }
 
 }
